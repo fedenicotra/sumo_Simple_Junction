8d3e07aecbeaeeada07e325100fdef37396013e5
fedenicotra <102322279+fedenicotra@users.noreply.github.com>
2023-02-20 11:53:04 +0100

    Add files via upload

--- test_semaphores.py
+++ test_semaphores.py
@@ -7,81 +7,60 @@
 import traci
 
 ## Setup commands to execute sumo (or sumo-gui eventually)
-cmdSumo_ =   ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/2023-01-05-18-11-50/osm.sumocfg"] #created with osm web wizard
+cmdSumo_OSM =   ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/2023-01-05-18-11-50/osm.sumocfg"] #created with osm web wizard
 cmdSumo_S =  ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/Semaforo/incrocioSemplice.sumocfg"]
 cmdSumo_A =  ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/Semaforo/incrocioSempliceA.sumocfg"]
 cmdSumo_AM = ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/Semaforo/incrocioSempliceAM.sumocfg"]
 
 ## Choose one of the previous commands
-cmdSumo = cmdSumo_A
+cmdSumo = cmdSumo_AM
 
+## Set your program IDs
+prog_numbers = ["0", "1", "2"]
+# prog_numbers = ["0"]
 
 traffic_light_ID = "J1"
 # traffic_light_ID = "436205045"
 
-test1 = test2 = test3 = 0
+## Every element will contain the "simulation end time"
+results = []
 
-## Traffic scale factor 
+## Traffic scale factor
+## "scale" variable turns on the scale factor for the traffic
 scale = False
 scale_factor = "1.5"
 
-## Program IDs
-first_program_number = "0"
-second_program_number = "1"
-third_program_number = "2"
-
 ################
 
-## Loading simulation
-traci.start(cmdSumo)
-## Setting up the traffic light program
-traci.trafficlight.setProgram(traffic_light_ID, first_program_number)
-print("RUNNING SIM. PROG: ", traci.trafficlight.getProgram(traffic_light_ID))
-## Setting scale factor
-if scale : 
-    traci.simulation.setScale(scale_factor)
+for prog_number in prog_numbers:
+    ## Loading simulation
+    traci.start(cmdSumo)
 
-## Running simulation ##
-## Loops until all vehicles in the configuration had arrived
-## The command   traci.simulationStep()   steps forword the simulation.
-while traci.simulation.getMinExpectedNumber() > 0:
-    traci.simulationStep()
+    ## Setting up the traffic light program
+    traci.trafficlight.setProgram(traffic_light_ID, prog_number)
+    print("RUNNING SIM. PROG: ", traci.trafficlight.getProgram(traffic_light_ID))
 
-## Saves simulation time
-test1 = traci.simulation.getTime()
+    ## Setting scale factor
+    if scale : 
+        traci.simulation.setScale(scale_factor)
+    
+    ## Running simulation ##
+    ## Loops until all vehicles in the configuration had arrived
+    ## The command   traci.simulationStep()   steps forword the simulation.
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
 
-## Close simulation
-traci.close()
+    ## Saves simulation time
+    results.append([prog_number, traci.simulation.getTime()])
 
-################
+    ## Close simulation
+    traci.close()
 
-traci.start(cmdSumo)
-traci.trafficlight.setProgram(traffic_light_ID, second_program_number)
-print("RUNNING SIM. PROG: ", traci.trafficlight.getProgram(traffic_light_ID))
-if scale : 
-    traci.simulation.setScale(scale_factor)
-while traci.simulation.getMinExpectedNumber() > 0:
-    traci.simulationStep()
+output_str = "\n"
+if scale:
+    output_str = output_str + "Scale factor: " + scale_factor + "\n"
 
-test2 = traci.simulation.getTime()
-traci.close()
+for result in results:
+    output_str = output_str + "Prog. " + result[0] + ": " + str(result[1]) + " s\n"
 
-################
-
-traci.start(cmdSumo)
-traci.trafficlight.setProgram(traffic_light_ID, third_program_number)
-print("RUNNING SIM. PROG: ", traci.trafficlight.getProgram(traffic_light_ID))
-if scale : 
-    traci.simulation.setScale(scale_factor)
-while traci.simulation.getMinExpectedNumber() > 0:
-    traci.simulationStep()
-
-test3 = traci.simulation.getTime()
-traci.close()
-
-################
-
-print("PROG ", first_program_number,": " , test1, 
-    "\nPROG ", second_program_number, ": ", test2,
-    "\nPROG ", third_program_number, ": ", test3, sep='')
-# print("PROG 0: ", test1)
+print(output_str)
--- vel&acc.py
+++ vel&acc.py
@@ -11,23 +11,22 @@ import matplotlib.pyplot as plt
 cmdSumo_osm = ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/2023-01-05-18-11-50/osm.sumocfg"]
 cmdSumo_S  =  ["sumo", "-c", "C:/Users/Federico Nicotra/Sumo/Semaforo/incrocioSemplice.sumocfg"]
 
-cmdSumo = cmdSumo_S
+cmdSumo = cmdSumo_osm
 traci.start(cmdSumo)
-vehicle = "t_1.0"
+vehicle = "veh0"
 sp = []
 ac = []
 found = False
 steps = 0
 while traci.simulation.getMinExpectedNumber() > 0:
     vehicles = traci.vehicle.getIDList()
-    # if steps % 50: print(vehicles)
     if vehicle in vehicles:
         s = traci.vehicle.getSpeed(vehicle)
         a = traci.vehicle.getAcceleration(vehicle)
         if(s >= 0): 
             sp.append(s)
+            ac.append(a)
             found = True
-        ac.append(a)
     else:
         if found:
             break
@@ -35,9 +34,9 @@ while traci.simulation.getMinExpectedNumber() > 0:
     steps += 1
 
 traci.close()
+
 x_sp = np.arange(0, len(sp), 1)
 x_ac = np.arange(0, len(ac), 1)
-
 plt.plot(x_sp, sp, 'b-', label='Speed (m/s)')
 plt.plot(x_ac, ac, 'r-', label='Acceleration (m/s^2)')
 plt.legend(loc='best')
